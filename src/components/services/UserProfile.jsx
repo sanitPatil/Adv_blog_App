2edfafae5f8ff7b6cc4a617e8fe023bd1543bf0b
sanitPatil <raghavpatilrp5@gmail.com>
2024-09-21 16:48:13 +0530

    delete user account functionality complete

--- src/components/services/UserProfile.jsx
+++ src/components/services/UserProfile.jsx
@@ -1,10 +1,10 @@
-import { Link } from 'react-router-dom';
 import { useState } from 'react';
 import { useLoginStore } from '../../zustStore/Store';
 import { LoaderCircle, Pencil, UserRoundPen } from 'lucide-react';
 import Input from '../Elements/Input';
 import { useForm } from 'react-hook-form';
-import { storageService } from '../../index';
+import { authService, storageService } from '../../index';
+import { useNavigate } from 'react-router-dom';
 
 function UserProfile() {
   const [activeIndex, setActiveIndex] = useState(0);
@@ -65,6 +65,7 @@ function UserProfile() {
 }
 
 export default UserProfile;
+// ***********************************************************************************
 const PersonalUpdate = () => {
   const [edit, setEdit] = useState(false);
   const { loginUser } = useLoginStore((state) => state);
@@ -104,76 +105,78 @@ const PersonalUpdate = () => {
     reset();
   };
   return (
-    <form onSubmit={handleSubmit(userProfileData)}>
-      <div className="border">
-        <div className=" w-full h-full">
-          <h1 className="text-center text-3xl font-semibold mt-4">
-            User Profile Details
-          </h1>
-          <div className="grid grid-cols-2 gap-6 mt-2">
-            <div className="">
-              <div className="flex justify-end pr-10 pt-1">
-                {edit ? (
-                  <button
-                    type="submit"
-                    className="  border text-center w-24 p-1 rounded-lg text-white bg-black font-semibold"
-                  >
-                    {loading ? (
-                      <LoaderCircle className="animate-spin items-center mx-auto text-white" />
-                    ) : (
-                      'Update'
-                    )}{' '}
-                  </button>
-                ) : (
-                  <button
-                    onClick={() => setEdit((prev) => !prev)}
-                    className="flex justify-between border text-center w-20 p-1 rounded-lg text-white bg-black font-semibold"
-                  >
-                    Edit <Pencil />
-                  </button>
-                )}
+    <div>
+      {error && <p className="text-red-700 text-center">{error}</p>}
+      <form onSubmit={handleSubmit(userProfileData)}>
+        <div className="border">
+          <div className=" w-full h-full">
+            <h1 className="text-center text-3xl font-semibold mt-4">
+              User Profile Details
+            </h1>
+            <div className="grid grid-cols-2 gap-6 mt-2">
+              <div className="">
+                <div className="flex justify-end pr-10 pt-1">
+                  {edit ? (
+                    <button
+                      type="submit"
+                      className="  border text-center w-24 p-1 rounded-lg text-white bg-black font-semibold"
+                    >
+                      {loading ? (
+                        <LoaderCircle className="animate-spin items-center mx-auto text-white" />
+                      ) : (
+                        'Update'
+                      )}{' '}
+                    </button>
+                  ) : (
+                    <button
+                      onClick={() => setEdit((prev) => !prev)}
+                      className="flex justify-between border text-center w-20 p-1 rounded-lg text-white bg-black font-semibold"
+                    >
+                      Edit <Pencil />
+                    </button>
+                  )}
+                </div>
+                <div className="flex justify-between p-1 m-2  ">
+                  <label className="p-1">Name</label>
+                  <input
+                    {...register('name')}
+                    disabled={true}
+                    type="text"
+                    className={`w-[70%]  rounded p-2  ${edit ? 'border' : ''}`}
+                  />
+                </div>
+                <div className="flex justify-between p-1 m-2">
+                  <label htmlFor="" className="p-1">
+                    user Name
+                  </label>
+                  <input
+                    {...register('username')}
+                    disabled={!edit}
+                    type="text"
+                    className={`w-[70%]  rounded p-2  ${edit ? 'border' : ''}`}
+                  />
+                </div>
+                <div className="flex justify-between p-1 m-2">
+                  <label htmlFor="" className="p-1">
+                    Bio
+                  </label>
+                  <input
+                    {...register('bio')}
+                    disabled={!edit}
+                    type="text"
+                    className={`w-[70%]  rounded p-2  ${edit ? 'border' : ''}`}
+                  />
+                </div>
               </div>
-              <div className="flex justify-between p-1 m-2  ">
-                <label className="p-1">Name</label>
-                <input
-                  {...register('name')}
-                  disabled={true}
-                  type="text"
-                  className={`w-[70%]  rounded p-2  ${edit ? 'border' : ''}`}
-                />
-              </div>
-              <div className="flex justify-between p-1 m-2">
-                <label htmlFor="" className="p-1">
-                  user Name
-                </label>
-                <input
-                  {...register('username')}
-                  disabled={!edit}
-                  type="text"
-                  className={`w-[70%]  rounded p-2  ${edit ? 'border' : ''}`}
-                />
-              </div>
-              <div className="flex justify-between p-1 m-2">
-                <label htmlFor="" className="p-1">
-                  Bio
-                </label>
-                <input
-                  {...register('bio')}
-                  disabled={!edit}
-                  type="text"
-                  className={`w-[70%]  rounded p-2  ${edit ? 'border' : ''}`}
-                />
-              </div>
-            </div>
-            <div className="flex justify-between p-2 m-2">
-              <div>
-                <UserRoundPen className="w-full h-full p-2 rounded-full border text-center" />
-              </div>
-              <div className="flex items-center">
-                <Input
-                  {...register('profilePicture')}
-                  label="choose Profile"
-                  className={`block w-full text-sm text-slate-500
+              <div className="flex justify-between p-2 m-2">
+                <div>
+                  <UserRoundPen className="w-full h-full p-2 rounded-full border text-center" />
+                </div>
+                <div className="flex items-center">
+                  <Input
+                    {...register('profilePicture')}
+                    label="choose Profile"
+                    className={`block w-full text-sm text-slate-500
                                 file:mr-4 file:py-2 file:px-4
                                 file:rounded-full file:border-0
                                 file:text-sm file:font-semibold
@@ -183,17 +186,20 @@ const PersonalUpdate = () => {
                                    ? 'hover:file:bg-gray-100'
                                    : ' hover:file:bg-violet-100 '
                                }`}
-                  type="file"
-                  disabled={!edit}
-                />
+                    type="file"
+                    disabled={!edit}
+                  />
+                </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
-    </form>
+      </form>
+    </div>
   );
 };
+
+// =************************************************************************************************
 const Security = () => {
   //security
   const [edit, setEdit] = useState(false);
@@ -304,14 +310,83 @@ const UpdatePassword = () => {
     </>
   );
 };
+
+////////////****************************************************************************** */
 const AccountDelete = () => {
+  const navigate = useNavigate();
+  const [showModal, setShowModal] = useState(false);
+  const { loginUser } = useLoginStore((state) => state);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState('');
+
+  const handleDeleteAccount = () => {
+    setError('');
+    setLoading(true);
+
+    authService
+      .deleteAccount(loginUser?.$id)
+      .then((res) => {
+        alert('account deleted succesfully');
+        setError('');
+        setLoading(false);
+        setShowModal(false);
+        navigate('/register');
+        return;
+      })
+      .catch((err) => {
+        setError(`${err.message}`);
+        setLoading(false);
+        setShowModal(false);
+
+        return;
+      });
+  };
   return (
     <>
-      <div className="border">
-        <h1 className=" text-center text-3xl text-red-700 font-semibold ">
-          Delete Your Account
-        </h1>
-        <div></div>
+      <div>
+        <div className="w-full h-full text-center m-4 font-bold text-red-700 text-3xl">
+          <h1>Delete Account</h1>
+        </div>
+
+        <div className="w-full h-full text-center">
+          <button
+            className="bg-red-700 p-2 text-white font-bold "
+            onClick={() => setShowModal(true)}
+          >
+            Delete Account
+          </button>
+        </div>
+        {showModal && (
+          <div className="modal">
+            <div className="modal-content text-red-700 p-2 text-center m-4">
+              <h2>Are you sure?</h2>
+              <p>
+                This action cannot be undone. You will permanently lose all your
+                data associated with this account.
+              </p>
+            </div>
+            <div className="w-full h-full text-red-700 text-center font-semibold">
+              <button
+                onClick={() => setShowModal(false)}
+                className="p-2 font-bold border text-white bg-red-700 rounded-lg m-2"
+              >
+                Cancel
+              </button>
+              <div className="w-full">
+                <button
+                  onClick={handleDeleteAccount}
+                  className="p-2 font-bold border w-30 h-full text-white bg-red-700 rounded-lg"
+                >
+                  {!loading ? (
+                    'Comfirm Delete'
+                  ) : (
+                    <LoaderCircle className="animate-spin" />
+                  )}
+                </button>
+              </div>
+            </div>
+          </div>
+        )}
       </div>
     </>
   );
